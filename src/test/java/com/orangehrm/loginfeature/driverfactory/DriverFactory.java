31702b73ecedf85bafc50d04d4016358d3b3dda5
chitranerlige <chitranr0@gmail.com>
2026-06-30 18:49:57 +0530

    fix: add headless mode and correct surefire xml path formatting

--- src/test/java/com/orangehrm/loginfeature/driverfactory/DriverFactory.java
+++ src/test/java/com/orangehrm/loginfeature/driverfactory/DriverFactory.java
@@ -2,7 +2,9 @@ package com.orangehrm.loginfeature.driverfactory;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.edge.EdgeOptions;
 import org.openqa.selenium.support.events.EventFiringDecorator;
 
 import com.orangehrm.loginfeature.listeners.HandleEvents;
@@ -19,12 +21,28 @@ public class DriverFactory {
 		
 		WebDriver driver = switch(browser.toLowerCase()) {
 
-	        case "chrome" -> new ChromeDriver();
+		case "chrome" -> {
+			// Configure Chrome options for Headless execution on CI/CD
+			ChromeOptions chromeOptions = new ChromeOptions();
+			chromeOptions.addArguments("--headless=new");
+			chromeOptions.addArguments("--no-sandbox");
+			chromeOptions.addArguments("--disable-dev-shm-usage");
+			chromeOptions.addArguments("--window-size=1920,1080");
+			yield new ChromeDriver(chromeOptions);
+		}
 
-	        case "edge" -> new EdgeDriver();
+		case "edge" -> {
+			// Configure Edge options for Headless execution on CI/CD
+			EdgeOptions edgeOptions = new EdgeOptions();
+			edgeOptions.addArguments("--headless");
+			edgeOptions.addArguments("--no-sandbox");
+			edgeOptions.addArguments("--disable-dev-shm-usage");
+			edgeOptions.addArguments("--window-size=1920,1080");
+			yield new EdgeDriver(edgeOptions);
+		}
 
-	        default -> throw new IllegalArgumentException(
-                    "Unsupported browser: " + browser);
+		default -> throw new IllegalArgumentException(
+				"Unsupported browser: " + browser);
 
 	    };
 	    return new EventFiringDecorator<>(
